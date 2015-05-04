483ef7856267c58ad69f218807a6a2e91f3601a2
lelect <lelect@me.com>
2015-05-05 00:30:02 +0900

    Add NSStreamDelegate to ViewController Add GCD **import CoreFoundation

--- Swift TCP/ViewController.swift
+++ Swift TCP/ViewController.swift
@@ -6,18 +6,78 @@
 //  Copyright (c) 2015年 test. All rights reserved.
 //
 
+import CoreFoundation;
 import UIKit
 
-class ViewController: UIViewController {
+func printQueueLabel(function:String = __FUNCTION__){
+    let label = dispatch_queue_get_label(DISPATCH_CURRENT_QUEUE_LABEL);
+    NSLog("\(function)@%s",label);
+}
+
+class ViewController: UIViewController, NSStreamDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        initTcpNetwork();
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func initTcpNetwork() {
+        printQueueLabel();
+        var readStream:  Unmanaged<CFReadStream>?
+        var writeStream: Unmanaged<CFWriteStream>?
+
+        CFStreamCreatePairWithSocketToHost(nil, "127.0.0.1", 5000, &readStream, &writeStream);
+
+        var inputStream: NSInputStream = readStream!.takeRetainedValue();
+        var outputStream: NSOutputStream = writeStream!.takeRetainedValue();
+
+        inputStream.delegate = self;
+        outputStream.delegate = self;
+
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)){
+            let loop = NSRunLoop.currentRunLoop();
+            inputStream.scheduleInRunLoop(loop, forMode: NSDefaultRunLoopMode);
+            outputStream.scheduleInRunLoop(loop, forMode: NSDefaultRunLoopMode);
+            loop.run();
+        }
+
+        //Open Streams
+        inputStream.open()
+        outputStream.open()
+
+    }
+
+    func stream(aStream: NSStream, handleEvent eventCode: NSStreamEvent) {
+        printQueueLabel();
+        switch (eventCode){
+        case NSStreamEvent.OpenCompleted:
+            NSLog("Stream opened");
+            break
+        case NSStreamEvent.HasBytesAvailable:
+            var inputstream = aStream as? NSInputStream;
+
+            var buffer = [UInt8](count: 4096, repeatedValue: 0);
+            while ((inputstream?.hasBytesAvailable) != nil) {
+                var len = inputstream?.read(&buffer, maxLength: 4096);
+                if (len > 0) {
+                    var output: NSString = NSString(bytes:&buffer, length:len!, encoding:NSASCIIStringEncoding)!;
+                    recv(output);
+                }
+            }
+            break
+        case NSStreamEvent.ErrorOccurred:
+            NSLog("ErrorOccurred")
+            break
+        case NSStreamEvent.EndEncountered:
+            NSLog("EndEncountered")
+            break
+        default:
+            NSLog("unknown.")
+        }
+    }
+    
+    func recv(recv: NSString) {
+        print(recv);
     }
 
 
