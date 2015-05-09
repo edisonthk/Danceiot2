66934748b02dca14d19b14369404f817c1c28045
edisonthk-mac <edisonthk@gmail.com>
2015-05-09 23:00:51 +0900

    Fix to write data

--- Swift TCP/ViewController.swift
+++ Swift TCP/ViewController.swift
@@ -13,51 +13,72 @@ class ViewController: UIViewController, NSStreamDelegate {
     
 
     @IBOutlet weak var button: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         initTcpNetwork();
         self.button.setTitle("Test", forState: UIControlState.Normal);
+        
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)){
+            while(true){
+                self.writeString("hello\n");
+                sleep(1);
+            }
+        }
+        
     }
 
     func printQueueLabel(function:String = __FUNCTION__){
         let label = dispatch_queue_get_label(DISPATCH_CURRENT_QUEUE_LABEL);
         println("\(function) @ \(String.fromCString(label)!)");
     }
-
+    
+    
+    var outputStream:NSOutputStream?
+    var inputStream:NSInputStream?
+    var outputQueue: NSOperationQueue{ get{
+        let ret = NSOperationQueue()
+        ret.maxConcurrentOperationCount = 1
+        return ret
+        }}
+    
     func initTcpNetwork() {
-        printQueueLabel();
-        var readStream:  Unmanaged<CFReadStream>?
-        var writeStream: Unmanaged<CFWriteStream>?
 
-        CFStreamCreatePairWithSocketToHost(nil, "127.0.0.1", 55222, &readStream, &writeStream);
+        NSStream.getStreamsToHostWithName("127.0.0.1", port: 8081, inputStream: &inputStream, outputStream: &outputStream)
 
-        var inputStream: NSInputStream = readStream!.takeRetainedValue();
-        var outputStream: NSOutputStream = writeStream!.takeRetainedValue();
-
-        inputStream.delegate = self;
-        outputStream.delegate = self;
+        inputStream?.delegate = self;
+        outputStream?.delegate = self;
 
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)){
             self.printQueueLabel();
             let loop = NSRunLoop.currentRunLoop();
-            inputStream.scheduleInRunLoop(loop, forMode: NSDefaultRunLoopMode);
-            outputStream.scheduleInRunLoop(loop, forMode: NSDefaultRunLoopMode);
-            inputStream.open()
-            outputStream.open()
+            self.inputStream?.scheduleInRunLoop(loop, forMode: NSDefaultRunLoopMode);
+            self.outputStream?.scheduleInRunLoop(loop, forMode: NSDefaultRunLoopMode);
+            self.inputStream?.open()
+            self.outputStream?.open()
             loop.run();
         }
 
     }
+    
+    func writeData() {
+        self.printQueueLabel();
+    }
 
     @IBAction func touchUpInside(sender: AnyObject) {
-        printQueueLabel();
+        println("touch up");
     }
 
     func stream(aStream: NSStream, handleEvent eventCode: NSStreamEvent) {
-        printQueueLabel();
+
         switch (eventCode){
         case NSStreamEvent.OpenCompleted:
             NSLog("Stream opened%@", NSStringFromClass(aStream.dynamicType));
+            break
+        case NSStreamEvent.HasSpaceAvailable:
             break
         case NSStreamEvent.HasBytesAvailable:
             var inputstream = aStream as? NSInputStream;
@@ -83,12 +104,35 @@ class ViewController: UIViewController, NSStreamDelegate {
     }
     
     func recv(recv: NSString) {
-        printQueueLabel();
         dispatch_async(dispatch_get_main_queue()) {
+            println(recv);
             self.button.setTitle(recv as String, forState: UIControlState.Normal);
         }
     }
-
+    
+    func writeString(str: String) {
+        writeData(str.dataUsingEncoding(NSUTF8StringEncoding)!)
+    }
+    ///write binary data to the TCPSocket. This sends it as a binary frame.
+    func writeData(data: NSData) {
+        while true {
+            if((outputStream?.hasSpaceAvailable) != nil) {
+                break;
+            }
+        }
+        outputStream?.write(UnsafePointer<UInt8>(data.bytes), maxLength: data.length);
+    }
+    
+    ///used to write things to the stream in a
+    private func enqueueOutput(data: NSData) {
+        outputQueue.addOperationWithBlock {
+            if let stream = self.outputStream{
+                if(stream.hasSpaceAvailable){
+                    stream.write(UnsafePointer<UInt8>(data.bytes), maxLength: data.length)
+                }
+            }
+        }
+    }
 
 }
 
